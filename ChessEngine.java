b903072b4a5d3f204f2ff183a03308835c8920c7
r8025n <raihanshovon95@gmail.com>
2021-11-01 17:24:50 +0600

    Implement alpha beta pruning algorithm

--- ChessEngine.java
+++ ChessEngine.java
@@ -3,45 +3,123 @@ import java.util.Random;
 
 public class ChessEngine {
 	
+	static int globalDepth = 4;
+
 	static String calculateComputerMoves() {
 		String allComputerMoves = "";
 
 		for(int i = 0; i < 8; i++) {
 			for(int j = 0; j < 8; j++) {
-				if(Character.isLowerCase(Board.boardArray[i][j].charAt(0)))
-					allComputerMoves += Board.spots[i][j].occupyingPiece.possibleMoves(i, j);
+				if(Character.isLowerCase(Board.boardArray[i][j].charAt(0))){
+					// System.out.println("lowerCase paisi");
+					allComputerMoves += MoveGeneration.returnPossibleMoves(i, j);
+				}
 			}
 		}
-		System.out.println("Computer Moves = " + allComputerMoves);
+		// System.out.println("Computer Moves = " + allComputerMoves);
 		return allComputerMoves;
 	}
 
-	static String alphaBeta() {
-		String s = calculateComputerMoves();
-		int i;
-		int limit = s.length()/5;
-		Random random = new Random();
-		i = random.nextInt(limit);
-		// i = limit - 1;
-		System.out.println("random number = " + i);
-		int si = i*5, ei = si + 5;
-		String answer = s.substring(si, ei);
-		System.out.println("random move = " + answer);
+	// static String alphaBeta() {
+	// 	String s = calculateComputerMoves();
+	// 	int i;
+	// 	int limit = s.length()/5;
+	// 	Random random = new Random();
+	// 	i = random.nextInt(limit);
+	// 	// i = limit - 1;
+	// 	System.out.println("random number = " + i);
+	// 	int si = i*5, ei = si + 5;
+	// 	String answer = s.substring(si, ei);
+	// 	System.out.println("random move = " + answer);
 
-		return answer;
-	}
-
-	// static string alphabeta(int depth, int alpha, int beta) {
-	// 	String str = calculateComputerMoves();
-
-	// 	if(depth == 0 || str.size() == 0)
-	// 		return
-
-		
+	// 	return answer;
 	// }
 
+	static String makeMove(String move) {
+		String temp = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
+		//Piece tempPiece= Board.spots[move.charAt(2) - '0'][move.charAt(3) - '0'].occupyingPiece;
+		String returnString = move.substring(4);
+		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'];
+		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = " ";
+		// Board.printBoardArray();
+		return returnString;
+	}
+
+	static void undoMove(String move, String temp) {
+		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
+		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = temp;
+	}
+
+	static void flipBoard() {
+		String temp = "";
+
+		for(int i = 0; i < 32; i++){
+			int r = i/8 , c = i%8 ; 
+			temp = Board.boardArray[r][c];
+			Board.boardArray[r][c] = flipCase(Board.boardArray[7-r][7-c]);
+			Board.boardArray[7-r][7-c] = flipCase(temp);
+		}
+		// System.out.println("After flip: ");
+		//Board.printBoardArray();
+	}
+
+	static String flipCase(String str){
+		char c = str.charAt(0);
+		String ret = "";
+
+		if(Character.isUpperCase(c))
+			ret = Character.toString(Character.toLowerCase(c));
+		else
+			ret = Character.toString(Character.toUpperCase(c));
+
+		return ret;
+	}
+
+	static String alphaBeta(int depth, int alpha, int beta, String move, int player) {
+		String moves = calculateComputerMoves();
+
+		if(depth == 0 || moves.length() == 0)
+			return move + Rating.rating();
+		player = 1 - player;
+
+		for(int i = 0; i < moves.length(); i+=5) {
+			String temp = makeMove(moves.substring(i, i + 5));
+			flipBoard();
+			String returnString = alphaBeta(depth - 1, alpha, beta, moves.substring(i, i + 5), player);
+			int value = Integer.valueOf(returnString.substring(5));
+			// String move = returnString.subString(0, 5);
+			flipBoard();
+			undoMove(moves.substring(i, i + 5), temp);
+			
+			if(player == 1){
+				if(alpha < value) {
+					alpha = value;
+					if(depth == globalDepth)
+						move = returnString.substring(0, 5);
+				}
+			}
+			else{
+				if(beta > value) {
+					beta = value;
+					if(depth == globalDepth)
+						move = returnString.substring(0, 5);
+				}
+			}
+			if(alpha >= beta){
+				if(player == 0)
+					return move + beta;
+				else
+					return move + alpha;
+			}
+		}
+		if(player == 0)
+			return move + beta;
+		else
+			return move + alpha;
+	}
+
 	static void makeComputerMove() {
-		String move  = alphaBeta();
+		String move  = alphaBeta(globalDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, " ", 0);
 		System.out.println(move);
 		int sourceX = move.charAt(0) - '0';
 		int sourceY = move.charAt(1) - '0';
@@ -57,6 +135,7 @@ public class ChessEngine {
 		targetSpot.pieceColor = Board.computerColor;
 		sourceSpot.toggleEmpty();
 		targetSpot.toggleEmpty();
+		
 		if(capturedPiece != ' ')
 			targetSpot.toggleEmpty();
 
--- Rating.java
+++ Rating.java
@@ -1,5 +1,42 @@
 public class Rating {
-	int rating() {
-		return 0;
+
+	static int rating() {
+		int rate = 0;
+		rate += rateState();
+		ChessEngine.flipBoard();
+		rate -= rateState();
+		ChessEngine.flipBoard();
+
+		return (rate * -1);
+	}
+
+	static int rateState() {
+		int counter = 0; 
+		int bishopCounter = 0;
+
+		for(int i = 0; i <= 63; i++) {
+			int r = i/8, c = i%8;
+
+			switch(Board.boardArray[r][c]) {
+				case "P": counter += 100;
+					break;
+                case "R": counter += 500;
+                    break;
+                case "K": counter += 300;
+                    break;
+                case "B": bishopCounter += 1;
+                    break;
+                case "Q": counter += 900;
+                    break;
+			}			
+		}
+
+		if (bishopCounter>=2)
+            counter+=300*bishopCounter;
+        else
+            if(bishopCounter==1)
+            	counter+=250;
+
+        return counter;
 	} 
 }
