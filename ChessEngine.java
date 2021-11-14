5953b4af57e2404b7a4b615bba540bbefd8e4736
r8025n <raihanshovon95@gmail.com>
2021-11-14 19:21:23 +0600

    Add king safety checking while generating move in ChessEngine class

--- ChessEngine.java
+++ ChessEngine.java
@@ -4,50 +4,90 @@ import java.util.Random;
 public class ChessEngine {
 	
 	static int globalDepth = 4;
-
 	static String calculateComputerMoves() {
 		String allComputerMoves = "";
+		boolean kingSafe = true;
+		// if(Game.turn == 1){
+		// 	flipBoard();
+		// 	kingSafe = King.isKingSafe(7-King.king_X_Comp, 7-King.king_Y_Comp);
+		// 	flipBoard();
+		// 	Game.turn = 0;	
+		// }
+		// else{
+		// 	kingSafe = King.isKingSafe(King.king_X_Human, King.king_Y_Human);
+		// 	Game.turn = 1;
+		// }
+		
+		flipBoard();
+		kingSafe = King.isKingSafe(7-King.king_X_Comp, 7-King.king_Y_Comp);
+		flipBoard();
 
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				if(Character.isLowerCase(Board.boardArray[i][j].charAt(0))){
-					// System.out.println("lowerCase paisi");
-					allComputerMoves += MoveGeneration.returnPossibleMoves(i, j);
+		if(! kingSafe){
+			allComputerMoves += MoveGeneration.kingPossibleMoves(King.king_X_Comp, King.king_Y_Comp);	
+			// if(allComputerMoves.length() == 0)
+			// 	board.setGameOver();
+		}
+		else{
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					if(Character.isLowerCase(Board.boardArray[i][j].charAt(0))){
+						allComputerMoves += MoveGeneration.returnPossibleMoves(i, j);
+					}
 				}
 			}
 		}
-		// System.out.println("Computer Moves = " + allComputerMoves);
+
 		return allComputerMoves;
 	}
 
-	// static String alphaBetaPruning() {
-	// 	String s = calculateComputerMoves();
-	// 	int i;
-	// 	int limit = s.length()/5;
-	// 	Random random = new Random();
-	// 	i = random.nextInt(limit);
-	// 	// i = limit - 1;
-	// 	System.out.println("random number = " + i);
-	// 	int si = i*5, ei = si + 5;
-	// 	String answer = s.substring(si, ei);
-	// 	System.out.println("random move = " + answer);
+	static String alphaBetaPruning(int depth, int alpha, int beta, String move, int player) {
+		String moves = calculateComputerMoves();
+		System.out.println("Computer Moves "+ moves);
 
-	// 	return answer;
-	// }
+		if(depth == 0 || moves.length() == 0){
+			// System.out.println("at return logic " + move + ""+ Rating.rating());
+			return move + Rating.rating();
+		}
+		player = 1 - player;
 
-	static String makeMove(String move) {
-		String temp = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
-		//Piece tempPiece= Board.spots[move.charAt(2) - '0'][move.charAt(3) - '0'].occupyingPiece;
-		String returnString = move.substring(4);
-		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'];
-		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = " ";
-		// Board.printBoardArray();
-		return returnString;
-	}
-
-	static void undoMove(String move, String temp) {
-		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
-		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = temp;
+		for(int i = 0; i < moves.length(); i+=5) {
+			String temp = makeMove(moves.substring(i, i + 5));
+			flipBoard();
+			String returnString = alphaBetaPruning(depth - 1, alpha, beta, moves.substring(i, i + 5), player);
+			int value = Integer.valueOf(returnString.substring(5));
+			// String move = returnString.subString(0, 5);
+			flipBoard();
+			undoMove(moves.substring(i, i + 5), temp);
+			
+			if(player == 1){
+				if(alpha < value) {
+					alpha = value;
+					if(depth == globalDepth){
+						move = returnString.substring(0, 5);
+						System.out.println("final move = " + move);
+					}
+				}
+			}
+			else{
+				if(beta > value) {
+					beta = value;
+					if(depth == globalDepth){
+						move = returnString.substring(0, 5);
+						System.out.println("final move = " + move);
+					}
+				}
+			}
+			if(alpha >= beta){
+				if(player == 0)
+					return move + beta;
+				else
+					return move + alpha;
+			}
+		}
+		if(player == 0)
+			return move + beta;
+		else
+			return move + alpha;
 	}
 
 	static void flipBoard() {
@@ -73,47 +113,29 @@ public class ChessEngine {
 		return ret;
 	}
 
-	static String alphaBetaPruning(int depth, int alpha, int beta, String move, int player) {
-		String moves = calculateComputerMoves();
+	static String makeMove(String move) {
+		// String temp = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
+		//Piece tempPiece= Board.spots[move.charAt(2) - '0'][move.charAt(3) - '0'].occupyingPiece;
+		// if(Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'].equals("A") && Game.turn == 0){
+		// 	King.king_X_Human = move.charAt(0) - '0';
+		// 	King.king_Y_Human = move.charAt(1) - '0';
+		// }
 
-		if(depth == 0 || moves.length() == 0)
-			return move + Rating.rating();
-		player = 1 - player;
+		// if(Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'].equals("A") && Game.turn == 1){
+		// 	King.king_X_Comp = move.charAt(0) - '0';
+		// 	King.king_Y_Comp = move.charAt(1) - '0';
+		// }
 
-		for(int i = 0; i < moves.length(); i+=5) {
-			String temp = makeMove(moves.substring(i, i + 5));
-			flipBoard();
-			String returnString = alphaBetaPruning(depth - 1, alpha, beta, moves.substring(i, i + 5), player);
-			int value = Integer.valueOf(returnString.substring(5));
-			// String move = returnString.subString(0, 5);
-			flipBoard();
-			undoMove(moves.substring(i, i + 5), temp);
-			
-			if(player == 1){
-				if(alpha < value) {
-					alpha = value;
-					if(depth == globalDepth)
-						move = returnString.substring(0, 5);
-				}
-			}
-			else{
-				if(beta > value) {
-					beta = value;
-					if(depth == globalDepth)
-						move = returnString.substring(0, 5);
-				}
-			}
-			if(alpha >= beta){
-				if(player == 0)
-					return move + beta;
-				else
-					return move + alpha;
-			}
-		}
-		if(player == 0)
-			return move + beta;
-		else
-			return move + alpha;
+		String returnString = move.substring(4);
+		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'];
+		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = " ";
+		// Board.printBoardArray();
+		return returnString;
+	}
+
+	static void undoMove(String move, String temp) {
+		Board.boardArray[move.charAt(0) - '0'][move.charAt(1) - '0'] = Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'];
+		Board.boardArray[move.charAt(2) - '0'][move.charAt(3) - '0'] = temp;
 	}
 
 	// static void makeComputerMove() {
